0f681153c252d4cc249730b1f538f7c3939a5a99
Ben Weissmann <benweissmann@gmail.com>
2010-03-17 20:33:44 -0400

    licensing

--- test/src/lib/easy-rubygame.rb
+++ test/src/lib/easy-rubygame.rb
@@ -1,5 +1,24 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
+# Copyright (C) 2010 Ben Weissmann <benweissmann@gmail.com>
+#
+# This file is part of EasyRubygame.
+#
+# EasyRubygame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# EasyRubygame is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with EasyRubygame, in a file called COPYING.LESSER.
+# in addition, your should have received a copy of the GNU General
+# Public License, in a file called COPYING. If you did not
+# receive a copy of either of these documents, see
+# <http://www.gnu.org/licenses/>.
+
 require 'yaml'
 require 'lib/scenes'
 require 'lib/sprites'
--- test/src/src/main.rb
+++ test/src/src/main.rb
@@ -1,3 +1,24 @@
+# Copyright (C) 2010 Ben Weissmann <benweissmann@gmail.com>
+#
+# This file is part of EasyRubygame.
+#
+# EasyRubygame is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# EasyRubygame is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with EasyRubygame, in a file called COPYING.LESSER.
+# in addition, your should have received a copy of the GNU General
+# Public License, in a file called COPYING. If you did not
+# receive a copy of either of these documents, see
+# <http://www.gnu.org/licenses/>.
+
 paddle_one = Paddle.new(:up, :down, 0, 225)
 paddle_two = Paddle.new(:left, :right, 490, 225)
 ball = Ball.new(250, 250, 2, 2, paddle_one, paddle_two)
