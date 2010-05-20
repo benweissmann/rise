ff6bcc381a1d6e8877b7a30f9051755490563842
bobidden <mason@masonglidden.com>
2010-05-19 20:07:59 -0400

    removed unneeded folder

--- src-1/scenes/Named_Scene.rb
+++
@@ -1,7 +0,0 @@
-class Named_Scene < Scene
-  attr_accessor :name
-  def initialize(name, background = :white)
-    @name = name
-    super background
-  end
-end
--- src-1/sprites/.svn/text-base/NPC.rb.svn-base
+++
@@ -1,9 +0,0 @@
-class NPC < Sprite
-  attr_reader :text
-
-  def initialize (text)
-    super "person.gif"
-
-    @text = text
-  end
-end
